d33fd5da3f795046381b3ae7bdc84a8ee3fdc3ea
EpifantsevaM <75801353+EpifantsevaM@users.noreply.github.com>
2020-12-11 20:03:39 +0700

    Add files via upload

--- Qsort.c
+++ Qsort.c
@@ -11,54 +11,78 @@ void sort(int* a, int n){
     if(n < 2)
         return;
     int o = a[0];
+    int co = 1;
     int l = 1;
     int r = n - 1;
     while(l < r - 1){
-        while(a[l] < o && l != r - 1)
+        while(a[l] <= o && l != r - 1){
+            if(a[l] == o){
+                swap(a, co, l);
+                co++;
+            }
             l++;
-        while(a[r] > o && l != r - 1)
-            r--;
+        }
+        while(a[r] >= o && l != r - 1) {
+            if(a[r] == o){
+                swap(a, co, r);
+                co++;
+                if (co > l)
+                    l = co;
+            }
+            else
+                r--;
+        }
+
         if(a[l] > a[r]){
             swap(a, l, r);
         }
     }
+
+
     int v;
-    if (o > a[r]){
-        swap(a, r, 0);
-        v = r;
+    if(o > a[r]){
+        for(int i = 0; i < co; i++){
+            swap(a, r - i, i);
+        }
+        v = r - co + 1;
     }
     else if(o < a[l]){
-        swap(a, 0, l - 1);
-        v = l - 1;
+        for(int i = 0; i < co; i++){
+            swap(a, i, l - 1 - i);
+        }
+        v = l - co;
     }
     else{
-        swap(a, l, 0);
-        v = l;
+        for(int i = 0; i < co; i++){
+            swap(a, l - i, i);
+        }
+        v = l - co + 1;
     }
     sort(a, v);
-    sort(a + v + 1, n - v - 1);
+    sort(a + v + co, n - v - co);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    int n = 10;
-    int* a = malloc(sizeof(int) * 10);
-    a[0] = 8;
-    a[1] = 5;
-    a[2] = 13;
-    a[3] = 4;
-    a[4] = 0;
-    a[5] = 65;
-    a[6] = 789;
-    a[7] = 34;
-    a[8] = 2;
-    a[9] = 34;
+    if(argc != 3)
+        return 0;
+    FILE* fp = fopen(argv[1], "r");
+    if(fp == NULL)
+        return 1;
+    int n;
+    fscanf(fp, "%d", &n);
+    int a[n];
+    for(int i = 0; i < n; i++){
+        fscanf(fp, "%d", &a[i]);
+    }
+    fclose(fp);
     sort(a, n);
-    //for(int i = 0; i < 10; i++){
-       // printf("%d ", m[i]);
-    //}
-    for(int i = 0; i < 10; i++)
-        printf("%d ", a[i]);
-    free(a);
+    fp = fopen(argv[2], "w");
+    if(fp == NULL)
+        return 2;
+    for(int i = 0; i < n; i++){
+        fprintf(fp, "%d\n", a[i]);
+    }
+    fclose(fp);
     return 0;
 }
